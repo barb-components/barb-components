0f9b32955d08801d91fa1fc19d76e002d3b07526
Andy Weir <andy.weir@headforwards.com>
2020-06-09 19:47:53 +0100

    fix broken build

--- config/rollup.config.js
+++ config/rollup.config.js
@@ -1,34 +1,31 @@
 /* eslint-disable node/no-unsupported-features/es-syntax */
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
 import copy from 'rollup-plugin-copy';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import progress from 'rollup-plugin-progress';
 import sass from 'rollup-plugin-sass';
-import typescript from 'rollup-plugin-typescript2';
-
-import packageJson from '../package.json';
 
 export default {
   input: 'src/index.ts',
   output: [
     {
-      file: packageJson.main,
-      format: 'cjs',
-      sourcemap: true,
-    },
-    {
-      file: packageJson.module,
-      format: 'esm',
+      dir: 'build',
+      format: 'es',
+      exports: 'named',
       sourcemap: true,
     },
   ],
+  preserveModules: true,
   plugins: [
     progress({clearLine: false}),
     peerDepsExternal(),
     resolve(),
     commonjs(),
-    typescript({useTsconfigDeclarationDir: true}),
+    typescript({
+      exclude: ['src/AppFixture.tsx', '**/*.test.*', '**/*.fixture.*'],
+    }),
     commonjs(),
     sass({insert: true}),
     copy({
