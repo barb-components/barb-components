0894eda878d2bad12024a204fd38ba2f802b6840
Andy Weir <andy.weir@headforwards.com>
2020-06-09 21:29:21 +0100

    debug breaking build

--- config/rollup.config.js
+++ config/rollup.config.js
@@ -1,35 +1,45 @@
 /* eslint-disable node/no-unsupported-features/es-syntax */
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
-import typescript from '@rollup/plugin-typescript';
 import copy from 'rollup-plugin-copy';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import progress from 'rollup-plugin-progress';
 import sass from 'rollup-plugin-sass';
+import typescript from 'rollup-plugin-typescript2';
+
+const packageJson = require('../package.json');
 
 export default {
   input: 'src/index.ts',
   output: [
     {
-      dir: 'build',
-      format: 'es',
-      exports: 'named',
+      file: packageJson.main,
+      name: 'BarbComponents',
+      format: 'cjs',
+      sourcemap: true,
+    },
+    {
+      file: packageJson.module,
+      name: 'BarbComponents',
+      format: 'esm',
       sourcemap: true,
     },
   ],
-  preserveModules: true,
+  external: ['react', 'react-dom'],
   plugins: [
     progress({clearLine: false}),
     peerDepsExternal(),
     resolve(),
-    commonjs(),
     typescript({
-      exclude: [
-        'src/jest.setup.ts',
-        'src/AppFixture.tsx',
-        '**/*.test.*',
-        '**/*.fixture.*',
-      ],
+      useTsconfigDeclarationDir: true,
+      tsconfigOverride: {
+        exclude: [
+          '**/jest.setup.ts',
+          '**/AppFixture.tsx',
+          '**/*.test.*',
+          '**/*.fixture.*',
+        ],
+      },
     }),
     commonjs(),
     sass({insert: true}),
